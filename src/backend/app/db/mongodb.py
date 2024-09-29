a19c7b8265beaa4d95c192419126fb50981f307e
ItsVasconcellos <fernando.asv@uol.com.br>
2024-09-29 18:39:39 -0300

    feat: getDatabase

--- src/backend/app/db/mongodb.py
+++ src/backend/app/db/mongodb.py
@@ -10,5 +10,8 @@ class MongoDB:
     def get_collection(self, collection_name: str) -> Collection:
         return self.database[collection_name]
 
+    def get_database(self):
+        return self.database
+
     def close(self):
         self.client.close()
