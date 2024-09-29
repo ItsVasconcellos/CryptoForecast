cf8662f342d2bbe91849e34ffd82350db9413f3d
ItsVasconcellos <fernando.asv@uol.com.br>
2024-09-29 16:31:42 -0300

    fix: removing comments

--- src/backend/app/repositories/grid_repo.py
+++ src/backend/app/repositories/grid_repo.py
@@ -8,15 +8,12 @@ class GridFSRepository:
         self.fs = gridfs.GridFS(db)
 
     def save_model(self, filename: str, model_data: bytes) -> str:
-        """Saves the model to GridFS and returns the file ID as a string."""
         file_id = self.fs.put(model_data, filename=filename)
         return str(file_id)
 
     def get_model(self, file_id: str) -> bytes:
-        """Retrieves the model from GridFS by its file ID."""
         grid_out = self.fs.get(ObjectId(file_id))
         return grid_out.read()
 
     def delete_model(self, file_id: str):
-        """Deletes the model from GridFS by its file ID."""
         self.fs.delete(ObjectId(file_id))
