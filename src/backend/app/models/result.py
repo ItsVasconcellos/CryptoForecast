6ca095c740bce118370056dc7ef54d785e40fc99
ItsVasconcellos <fernando.asv@uol.com.br>
2024-09-29 16:50:58 -0300

    feat: predictions result model

---
+++ src/backend/app/models/result.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+
+class Result(BaseModel):
+    date: datetime = Field(..., description="Date of the prediction")
+    value: float = Field(..., description="Predicted value")
+
+    class Config:
+        json_schema_extra = {
+            "example": {"VALUE": "200", "DATA": "2023-10-01T12:00:00Z"}
+        }
