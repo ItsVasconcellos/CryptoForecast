e9732a21191a4e7ae5bc76345bd07197473b3a25
ItsVasconcellos <fernando.asv@uol.com.br>
2024-09-28 23:11:09 -0300

    feat: adding logic to handle mongo

--- src/backend/app/app.py
+++ src/backend/app/app.py
@@ -6,12 +6,37 @@ from app.db.mongodb import MongoDB
 
 from dotenv import dotenv_values
 
-app = FastAPI()
 
 config = dotenv_values(".env")
 
 DATABASE_URI = config.get("DATABASE_URI", "mongodb://db:27017")
 DATABASE_NAME = config.get("DATABASE_NAME", "cryptoForecast")
+
+
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    app.state.db = MongoDB(DATABASE_URI, DATABASE_NAME)
+
+    print("Connected to the MongoDB database!")
+
+    yield
+
+    app.state.db.close()
+    print("Disconnected to the MongoDB database!")
+
+
+app = FastAPI(lifespan=app_lifespan)
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
