2aa99bee331b238d341732f1585cb1c2e475404d
ItsVasconcellos <fernando.asv@uol.com.br>
2024-09-29 21:23:03 -0300

    feat: updating utils

--- src/backend/app/utils/crypto.py
+++ src/backend/app/utils/crypto.py
@@ -1,1 +1,5 @@
+from app.models.model import Model
+
 Crypto = ["SOL-USD", "BTC-USD", "ETH-USD", "LTC-USD", "XRP-USD", "ADA-USD", "DOT-USD"]
+
+Models_Crypto = [Model(model_name=crypto, gridfs_path="") for crypto in Crypto]
